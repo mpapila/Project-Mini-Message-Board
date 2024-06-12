90d36951b539f8b93f80f7e83c0c759789098c5c
mpapila <mehmetpapila95@gmail.com>
2024-06-12 14:03:24 -0500

    Implement basic message board functionality
    - Add form route and view for creating new messages
    - Display existing messages on the home page
    - Handle new message submission
    - Update README with project description

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
+
+router.post('/new', function (req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.user;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+})
 
 module.exports = router;
